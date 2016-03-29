be9e9feba2818f6edac812b05d53705be8381dce
Harald Wellmann <harald.wellmann@gmx.de>
2016-03-29 15:40:36 +0200

    simplifications for wildfly-swarm-custom profile

--- src/main/java/net/java/cargotracker/application/Main.java
+++ src/main/java/net/java/cargotracker/application/Main.java
@@ -1,12 +1,7 @@
-/*
- * Copyright 2016 EOS UPTRADE GmbH.
- *
- * This is proprietary software. All rights reserved. Unauthorized use is prohibited.
- */
 package net.java.cargotracker.application;
 
+import org.jboss.shrinkwrap.api.Archive;
 import org.wildfly.swarm.container.Container;
-import org.wildfly.swarm.undertow.WARArchive;
 
 /**
  * @author hwellmann
@@ -16,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Container container = new Container();
-        WARArchive deployment = container.createDefaultDeployment().as(WARArchive.class);//.addAllDependencies();
+        Archive<?> deployment = container.createDefaultDeployment();
         container.start().deploy(deployment);
     }
 
